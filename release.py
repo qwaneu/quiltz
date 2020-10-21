1520a23261c7fcbb0390aef491bc41e87e021516
rob <rob@qwan.eu>
2020-10-21 21:49:22 +0200

    add release script

---
+++ release.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import sys
+import re
+import subprocess
+sys.path.append('src')
+from quiltz.version import version
+
+new_version = sys.argv[1]
+old_version = version
+if not re.match(r'\d+\.\d+\.\d+',new_version):
+    print("invalid version: {}".format(new_version))
+
+answer = input('are you sure to release from {} to {}'.format(old_version, new_version))
+if answer != 'yes':
+    print('no confirmation')
+
+with open('src/quiltz/version.py', "w+") as file:
+    file.write('version="{}"'.format(new_version))
+
+def run(*cmdline):
+    print(cmdline)
+    #subprocess.run(splitted)    
+
+run("git", "pull")
+run("git", "add", ".")
+run("git", "commit", "-am", "release {}".format(new_version))
+run("git", "tag", "{}".format(new_version))
+run("git", "push")
+run("git", "push", "--tags")
